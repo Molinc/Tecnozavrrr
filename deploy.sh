7228c46190c2b4b945b448e650e69baa92d14056
Molinc <92622171+Molinc@users.noreply.github.com>
2024-07-11 15:34:00 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ cd dist
 
 git add -A
 git commit -m "deploy"
+git push -f git@github.com:Molinc/molinc.github.io/Tecnozavrrr1 master
 git push -f git@github.com:Molinc/Tecnozavrrr1.git master:gh-pages
 
 cd - 
